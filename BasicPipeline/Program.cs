b99c9e433effc04e42f35b583e3fa9846e2b782d
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-11-15 14:37:27 +0100

    Removed solution

--- BasicPipeline/Program.cs
+++ BasicPipeline/Program.cs
@@ -1,9 +1,4 @@
-﻿using BasicPipeline.Filters.Exercise0;
-using BasicPipeline.Framework;
-using BasicPipeline.InputOutput.Exercise0;
-using PrintResultFilter = BasicPipeline.Filters.Exercise1.PrintResultFilter;
-
-namespace BasicPipeline
+﻿namespace BasicPipeline
 {
     public class Program
     {
